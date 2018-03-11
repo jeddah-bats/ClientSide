108764588df8f1e65b45da2cd575c61ed40a879c
Turki BinTalib <turki.fcit@gmail.com>
2018-03-11 20:07:20 +0300

    Add files via upload

---
+++ src/components/Header.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { Carousel,Form } from 'react-bootstrap';
+
+class Header extends Component {
+    render() {
+        return (
+            <Carousel>
+                <Carousel.Item>
+                    <img width={900} height={500} alt="900x500" src="././public/assets/1.png" />
+                    <Carousel.Caption>
+                    <h3>First slide label</h3>
+                    <p>-------</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img width={900} height={500} alt="900x500" src="././public/assets/2.png" />
+                    <Carousel.Caption>
+                    <h3>Second slide label</h3>
+                    <p>--------</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
+        );
+    }
+
+}
+
+export default Header;
