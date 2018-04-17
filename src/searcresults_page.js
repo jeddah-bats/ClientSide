937044827c52256b0d1ccb0aacc3f4562ef6e28a
Turki BinTalib <turki.fcit@gmail.com>
2018-04-17 03:05:58 +0300

    Add files via upload

--- src/searcresults_page.js
+++ src/searcresults_page.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 import Navbar from './components/Navbar'; 
-import searcresults_page_navbar from './components/searcresults_page_navbar'; 
-import searcresults_page_page_grid from './components/searcresults_page_grid';
+import Searcresults_page_navbar from './components/searcresults_page_navbar'; 
+import Searcresults_page_grid from './components/searcresults_page_grid';
 
 class searcresults_page extends Component {
     render() {
         return (
             <div>
                 <Navbar/>
-                <searcresults_page_page_navbar data={this.props.match.params}/>
+                <Searcresults_page_navbar data={this.props.match.params}/>
                 <div id="griddiv">
-                    <searcresults_page_page_grid data={this.props.match.params}/>
+                    <Searcresults_page_grid data={this.props.match.params}/>
                 </div>
             </div>
         );
